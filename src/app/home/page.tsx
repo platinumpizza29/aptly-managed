4b53df0191481eae0fe6fe4e422bc69acaa301d8
platinumpizza29 <kbelgi1@gmail.com>
2024-03-31 22:32:51 +0100

    Feature update: Clerk Login and Register added

---
+++ src/app/home/page.tsx
@@ -0,0 +1,10 @@
+export default function HomePage() {
+  return (
+    <main className="min-w-screen min-h-screen">
+      <div className="navbar bg-base-100">
+        <a className="btn btn-ghost text-xl">Aptly</a>
+      </div>
+      <h1>Home Page</h1>
+    </main>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,19 @@
+import { auth, SignInButton } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
 export default function HomePage() {
+  const { userId } = auth();
+
+  if (userId) {
+    redirect("/home");
+  }
+
   return (
-    <main className="min-w-screen min-h-screen">Building management page</main>
+    <main className="min-w-screen min-h-screen">
+      <div className="navbar bg-base-100 flex flex-row items-center justify-between">
+        <a className="btn btn-ghost text-xl">Aptly</a>
+        <SignInButton redirectUrl="/home" />
+      </div>
+    </main>
   );
 }
